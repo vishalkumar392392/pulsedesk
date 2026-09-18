06786bd4eb2839cfa0025c1a73282b5a3364169c
Vishal palla <vishalgate2019@gmail.com>
2026-09-18 16:14:17 +0530

    added a dummy endpoint

--- pulsedesk/src/main/java/com/pulsedesk/controller/AuthController.java
+++ pulsedesk/src/main/java/com/pulsedesk/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -97,6 +98,11 @@ public class AuthController {
 		UserEntity saved = service.save(request);
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(ApiResponse.success(saved, "User Registered successfully", HttpStatus.CREATED.value()));
+	}
+	
+	@GetMapping("/test")
+	public String test() {
+		return "Started successfully";
 	}
 
 }
--- pulsedesk/src/main/java/com/pulsedesk/security/config/SecurityConfig.java
+++ pulsedesk/src/main/java/com/pulsedesk/security/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
 
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-						.requestMatchers("/auth/login", "/auth/refreshToken").permitAll()
+						.requestMatchers("/auth/login", "/auth/refreshToken", "/auth/test").permitAll()
 						.requestMatchers(HttpMethod.PATCH, "/internal/tickets/*/auto-assign").permitAll()
 //						.requestMatchers("/admin/**").hasAuthority("admin")
 						.anyRequest().authenticated())
